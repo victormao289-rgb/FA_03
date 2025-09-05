012e857f38dbb716de120fef9ff4b6df8fd1de91
Victor <victor@gmail.com>
2025-09-05 09:54:33 -0500

    subiendo ejercicio 1 de semana 3

---
+++ Semana 03_C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_03_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey(); //realizando detenimiento de consola
+        }
+
+
+        static void ejer1()
+        {
+           string nombre, carrera;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido al curso de Fundamentos de Algoritmos de {carrera}");
+        }
+
+    }
+}
---
+++ Semana 03_PY/Semana_03_PY.py
@@ -0,0 +1,8 @@
+def ejer1():
+    nombre = input("Ingrese su nombre: ");
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido al curso de Fundamentos de Algoritmos de {carrera}")
+
+ejer1()
+
